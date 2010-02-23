7d53187eb4d7cab3e3796e74f3fb9c730ad1198b
Andy Sturges <arsturges@lbl.gov>
2010-02-22 17:04:48 -0800

    removed authentication, fixed rulemaking view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
   layout 'application'
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
-  # uncomment the next line to enable authentication.
-  before_filter :login_required
+  # uncomment the next line to enable authentication. See also sessions_controller.
+  # before_filter :login_required
 
   # this may screw everything up. It may need to be in individual controllers.
   # May need to add an :except=>users or session or something.
