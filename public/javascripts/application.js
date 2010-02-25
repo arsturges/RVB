4e2c0f66c2309a2e3537b6e23696975c78291de5
Andy Sturges <arsturges@lbl.gov>
2010-02-25 15:27:06 -0800

    added CSS

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$('#extra_addresses li a[data-method="delete"]').live('ajax:before', 
+										function() {
+											$(this).parent().fadeOut();
+											$('#spinner').show();
+										}
+									);
+
+$('#extra_addresses li a[data-method="delete"]').live('ajax:complete', 
+										function() {
+											$('#spinner').hide();
+										}
+									);
+
+
+$('#new_member').live('ajax:before', function() {
+	$('#spinner').show();
+});
+										
+$('.listing > td.attribute').live('click', function() {
+	document.location = $(this).parent().attr('data-href');
+})
