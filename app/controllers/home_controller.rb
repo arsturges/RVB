7101ec92d7484a6e6722a0966034d6c94733be5a
Andy Sturges <arsturges@lbl.gov>
2010-04-16 20:29:46 -0700

    migrated to BluePrintCSS, added CSS themes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,14 @@ class HomeController < ApplicationController
 
   def index
     @search = Milestone.search(params[:search])
-    @started = Milestone.find(:all, :conditions => ["date_category_id = 5 AND milestone < ?  ", Date.today])
-    @underway = @search.all( :conditions => ["date_category_id = 6 AND milestone > ?  AND milestone < ? AND revision_number = ?", Date.today, 2.months.from_now, Revision.maximum(:revision_number) ])
+    @started = Milestone.find(:all, :include => [:rulemaking, :phase, :activity, :date_category], :conditions => ["date_category_id = 5 AND milestone < ?  ", Date.today])
+    @underway = @search.all( :include => [:rulemaking, :phase, :activity, :date_category, :revision], :conditions => ["date_category_id = 6 AND milestone > ?  AND milestone < ? AND revision_number = ?", Date.today, 2.months.from_now, Revision.maximum(:revision_number) ])
 
     @current = []
     @underway.each do |um|
     @current << um if @started.detect {|sm| sm.rulemaking_id = um.rulemaking_id  && sm.phase_id = um.phase_id && sm.activity_id = um.activity_id }
         end
-    @planned_finishes = Milestone.find(:all, :conditions => {:date_category_id => 2})
+    @planned_finishes = Milestone.find(:all, :include => [:rulemaking, :phase, :activity, :date_category, :revision], :conditions => {:date_category_id => 2})
 
   end
 
