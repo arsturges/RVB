8fb83ef5fa1ec587a92898f7062a5c7ee91b410a
Andy Sturges <arsturges@lbl.gov>
2010-04-26 09:43:00 -0700

    stylesheets and navigation

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,13 +10,10 @@ class HomeController < ApplicationController
     @current << um if @started.detect {|sm| sm.rulemaking_id = um.rulemaking_id  && sm.phase_id = um.phase_id && sm.activity_id = um.activity_id }
         end
     @planned_finishes = Milestone.find(:all, :include => [:rulemaking, :phase, :activity, :date_category, :revision], :conditions => {:date_category_id => 2})
-
   end
 
   def search
     @search = Milestone.search(params[:search])
     @milestone_data = params[:search] ? @search.all(:include => [:rulemaking, :phase, :activity, :date_category]) : [] #why don't I have to include :revision here?
-
-
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ ActionController::Routing::Routes.draw do |map|
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
 
-
   map.resources :products
 
   map.resources :sectors
